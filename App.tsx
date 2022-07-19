51553b621b9ad5965a466aa85d2a5c05c1a9143d
hugosmoreira <hugo.n4@gmail.com>
2022-07-18 21:32:08 -0700

    boilerplate react native expo tsx

--- App.tsx
+++ App.tsx
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
+import { NativeBaseProvider } from 'native-base';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import SignIn from './src/screens/SignIn';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <SignIn />
+    </NativeBaseProvider>
+    
   );
 }
 
---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,13 @@
+import  { View , Text } from 'react-native';
+
+type Props = {}
+
+function SignIn({}: Props) {
+  return (
+    <View>
+        <Text>SignIn</Text>
+    </View>
+  )
+}
+
+export default SignIn
