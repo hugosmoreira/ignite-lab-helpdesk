3d113cfd0e98bdfe9b8d04cda84f47dcb8350512
hugosmoreira <hugo.n4@gmail.com>
2022-07-23 19:39:05 -0700

    detail register components

--- src/screens/Details.tsx
+++ src/screens/Details.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { VStack, Text } from 'native-base';
+import { useRoute } from '@react-navigation/native';
 
-type Props = {}
+import { Header } from '../../components/Header';
 
-export default function Details({}: Props) {
+type RouteParams = {
+  orderId: string;
+}
+
+export function Details() {
+
+  const route = useRoute();
+  const { orderId } = route.params as RouteParams;
+
   return (
-    <div>Details</div>
-  )
+    <VStack flex={1} bg="gray.700">
+      <Header title="Solicitação" />
+      <Text color="white">
+        {orderId}
+      </Text>
+    </VStack>
+  );
 }
--- src/screens/Register.tsx
+++ src/screens/Register.tsx
@@ -3,14 +3,29 @@ import { VStack } from 'native-base';
 import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
-type Props = {}
 
-const Register = (props: Props) => {
+export function Register() {
   return (
-    <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24} >
-        <Header title="Register" />
-    </VStack>
-  )
-}
+    <VStack flex={1} p={6} bg="gray.600">
+      <Header title="Nova solicitação" />
 
-export default Register
+      <Input
+        placeholder="Número do patrimônio"
+        mt={4}
+      />
+
+      <Input
+        placeholder="Descrição do problema"
+        flex={1}
+        mt={5}
+        multiline
+        textAlignVertical="top"
+      />
+
+      <Button
+        title="Cadastrar"
+        mt={5}
+      />
+    </VStack>
+  );
+}
